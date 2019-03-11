a9e77de5be3c5c4388ba9c87bc61fbc24e812598
Alexey Nigin <nigin@umich.edu>
2019-03-11 19:19:51 -0400

    Add tests for -, *, /, and malformed expressions
    I wrote tests for subtraction, multiplication, and division, and also
    expanded the tests to check that the calculator correctly raises
    exceptions upon seeing a malformed expression. Since this project is
    TDD FTW(tm), all of these tests fail for now.

--- test_rpn.py
+++ test_rpn.py
@@ -2,6 +2,30 @@ import unittest
 import rpn
 
 class TestBasics(unittest.TestCase):
+	def test_invalid(self):
+		self.assertRaises(rpn.CalculatorError, rpn.calculate, "wazzup")
 	def test_add(self):
 		self.assertEqual(rpn.calculate("1 1 +"), 2)
 		self.assertEqual(rpn.calculate("-1.5 0 +"), -1.5)
+		self.assertRaises(rpn.CalculatorError, rpn.calculate, "1 +")
+		self.assertRaises(rpn.CalculatorError, rpn.calculate, "+")
+	def test_subtract(self):
+		self.assertEqual(rpn.calculate("2 1 -"), 1)
+		self.assertEqual(rpn.calculate("0 -1.5 -"), 1.5)
+		self.assertRaises(rpn.CalculatorError, rpn.calculate, "1 -")
+		self.assertRaises(rpn.CalculatorError, rpn.calculate, "-")
+	def test_multiply(self):
+		self.assertEqual(rpn.calculate("2 2 *"), 4)
+		self.assertEqual(rpn.calculate("0 0 *"), 0)
+		self.assertRaises(rpn.CalculatorError, rpn.calculate, "1 *")
+		self.assertRaises(rpn.CalculatorError, rpn.calculate, "*")
+	def test_divide(self):
+		self.assertEqual(rpn.calculate("4 2 /"), 2)
+		self.assertEqual(rpn.calculate("0 5 /"), 0)
+		self.assertRaises(rpn.CalculatorError, rpn.calculate, "1 0 /")
+		self.assertRaises(rpn.CalculatorError, rpn.calculate, "1 /")
+		self.assertRaises(rpn.CalculatorError, rpn.calculate, "/")
+	def test_nest(self):
+		self.assertEqual(rpn.calculate("6 2 2 + 4 1 - * + 2 /"), 9)
+		self.assertRaises(rpn.CalculatorError, rpn.calculate, "1 1")
+		self.assertRaises(rpn.CalculatorError, rpn.calculate, "1 1 + +")
